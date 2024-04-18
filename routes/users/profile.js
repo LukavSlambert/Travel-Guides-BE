1e85c8e377408cfa3242b4332276ea48a7eec23e
LukavSlambert <forspacereg@gmail.com>
2024-04-18 14:27:23 +0300

    get route done

---
+++ routes/users/profile.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const handleServerError = require('../../libs/utility/ErrorHandlers.js')
+const auth = require('../../libs/authentication.js')
+const userQuerys = require('../../database/userQuerys.js')
+
+
+router.get('/', auth.authenticate, async (req, res) => {
+    try {
+        const [user] = await userQuerys.GetUserByEmail(req.body.email)
+        if (!user) return res.status(400).send("User doesn't exist")
+
+        const password_valid = await bcrypt.compare(req.body.password, user.password);
+
+        if (!password_valid) {
+            res.statusMessage = "Email or password incorrect";
+            res.status(400).end();
+        } else {
+            res.send({ user })
+        }
+
+    } catch (err) {
+        handleServerError(err, res)
+    }
+})
+
+
+module.exports = router;
